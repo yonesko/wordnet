7f8d91271842fbb1f9e8f3bb2ec8a014c4fef34e
glebio <yonesko@gmail.com>
2019-10-14 20:36:48 +0300

    initHypernyms

--- WordNet.java
+++ WordNet.java
@@ -4,12 +4,31 @@
  *  Description:
  **************************************************************************** */
 
+import edu.princeton.cs.algs4.Digraph;
+import edu.princeton.cs.algs4.In;
+
 public class WordNet {
+
+    private Digraph hypernyms;
 
     // constructor takes the name of the two input files
     public WordNet(String synsets, String hypernyms) {
         if (synsets == null || hypernyms == null) {
             throw new IllegalArgumentException();
+        }
+        initHypernyms(hypernyms);
+    }
+
+    private void initHypernyms(String hypernyms) {
+        In in = new In(hypernyms);
+        String[] lines = in.readAllLines();
+        this.hypernyms = new Digraph(lines.length);
+        for (String line : lines) {
+            String[] split = line.split(",");
+            int synsetId = Integer.parseInt(split[0]);
+            for (int i = 1; i < split.length; i++) {
+                this.hypernyms.addEdge(synsetId, Integer.parseInt(split[1]));
+            }
         }
     }
 
@@ -44,6 +63,6 @@ public class WordNet {
     }
 
     public static void main(String[] args) {
-
+        WordNet wordNet = new WordNet("synsets.txt", "hypernyms.txt");
     }
 }
