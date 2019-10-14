df097ec5c0544d86b0a21a741be5fd1c4fee871a
glebio <yonesko@gmail.com>
2019-10-14 21:09:23 +0300

    Outcast

---
+++ Outcast.java
@@ -0,0 +1,20 @@
+/* *****************************************************************************
+ *  Name:
+ *  Date:
+ *  Description:
+ **************************************************************************** */
+
+public class Outcast {
+    public static void main(String[] args) {
+
+    }
+
+    public Outcast(WordNet wordnet) {
+        throw new UnsupportedOperationException("wordnet");
+    }
+
+    public String outcast(String[] nouns) {
+        throw new UnsupportedOperationException("outcast");
+    }
+
+}
