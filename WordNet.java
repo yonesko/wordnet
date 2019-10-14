c0f2c8db24c2b19284268ad92fe48b0a27e78a54
glebio <yonesko@gmail.com>
2019-10-14 15:08:47 +0300

    WordNet

--- WordNet.java
+++ WordNet.java
@@ -8,7 +8,9 @@ public class WordNet {
 
     // constructor takes the name of the two input files
     public WordNet(String synsets, String hypernyms) {
-
+        if (synsets == null || hypernyms == null) {
+            throw new IllegalArgumentException();
+        }
     }
 
     // returns all WordNet nouns
@@ -18,17 +20,26 @@ public class WordNet {
 
     // is the word a WordNet noun?
     public boolean isNoun(String word) {
+        if (word == null) {
+            throw new IllegalArgumentException();
+        }
         throw new UnsupportedOperationException();
     }
 
     // distance between nounA and nounB (defined below)
     public int distance(String nounA, String nounB) {
+        if (!isNoun(nounA) || !isNoun(nounB)) {
+            throw new IllegalArgumentException();
+        }
         throw new UnsupportedOperationException();
     }
 
     // a synset (second field of synsets.txt) that is the common ancestor of nounA and nounB
     // in a shortest ancestral path (defined below)
     public String sap(String nounA, String nounB) {
+        if (!isNoun(nounA) || !isNoun(nounB)) {
+            throw new IllegalArgumentException();
+        }
         throw new UnsupportedOperationException();
     }
 
