fcba31ce0713e1763663a438c1ef61970b798f16
glebio <yonesko@gmail.com>
2019-10-14 21:08:30 +0300

    distance

--- WordNet.java
+++ WordNet.java
@@ -7,17 +7,16 @@
 import edu.princeton.cs.algs4.Digraph;
 import edu.princeton.cs.algs4.In;
 
-import java.util.Collection;
 import java.util.Collections;
 import java.util.HashMap;
 import java.util.HashSet;
 import java.util.Set;
-import java.util.stream.Collectors;
 
 public class WordNet {
 
     private final Digraph hypernyms;
     private final HashMap<Integer, Set<String>> synsets;
+    private final HashMap<String, Set<Integer>> synsets2;
 
     // constructor takes the name of the two input files
     public WordNet(String synsetsFileName, String hypernymsFileName) {
@@ -26,6 +25,7 @@ public class WordNet {
         }
         hypernyms = buildHypernyms(hypernymsFileName);
         synsets = buildSynsets(synsetsFileName);
+        synsets2 = buildSynsets2(synsetsFileName);
     }
 
     private HashMap<Integer, Set<String>> buildSynsets(String synsetsFileName) {
@@ -36,6 +36,20 @@ public class WordNet {
         for (String line : lines) {
             String[] columns = line.split(",");
             result.put(Integer.parseInt(columns[0]), splitToNouns(columns[1]));
+        }
+        return result;
+    }
+
+    private HashMap<String, Set<Integer>> buildSynsets2(String synsetsFileName) {
+        In in = new In(synsetsFileName);
+        String[] lines = in.readAllLines();
+        HashMap<String, Set<Integer>> result = new HashMap<>(lines.length);
+
+        for (String line : lines) {
+            String[] columns = line.split(",");
+            for (String noun : splitToNouns(columns[1])) {
+                result.getOrDefault(noun, new HashSet<>()).add(Integer.parseInt(columns[0]));
+            }
         }
         return result;
     }
@@ -63,7 +77,7 @@ public class WordNet {
 
     // returns all WordNet nouns
     public Iterable<String> nouns() {
-        return synsets.values().stream().flatMap(Collection::stream).collect(Collectors.toList());
+        return synsets2.keySet();
     }
 
     // is the word a WordNet noun?
@@ -71,12 +85,7 @@ public class WordNet {
         if (word == null) {
             throw new IllegalArgumentException();
         }
-        for (Set<String> value : synsets.values()) {
-            if (value.contains(word)) {
-                return true;
-            }
-        }
-        return false;
+        return synsets2.containsKey(word);
     }
 
     // distance between nounA and nounB (defined below)
@@ -84,7 +93,8 @@ public class WordNet {
         if (!isNoun(nounA) || !isNoun(nounB)) {
             throw new IllegalArgumentException();
         }
-        throw new UnsupportedOperationException();
+
+        return new SAP(hypernyms).length(synsets2.get(nounA), synsets2.get(nounB));
     }
 
     // a synset (second field of synsets.txt) that is the common ancestor of nounA and nounB
@@ -93,7 +103,8 @@ public class WordNet {
         if (!isNoun(nounA) || !isNoun(nounB)) {
             throw new IllegalArgumentException();
         }
-        throw new UnsupportedOperationException();
+        return String.join(" ", synsets.get(
+                new SAP(hypernyms).ancestor(synsets2.get(nounA), synsets2.get(nounB))));
     }
 
     public static void main(String[] args) {
